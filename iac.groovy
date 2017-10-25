b33b77ad398c82d245d8c9697c5c2e574d3dac1c
Jayaprakash Reddy <JUSTPERFECT@users.noreply.github.com>
2017-10-25 18:51:56 +0530

    Update iac.groovy

--- iac.groovy
+++ iac.groovy
@@ -2,3 +2,21 @@ folder('project-a') {
     displayName('Project A')
     description('Folder for project A')
 }
+
+pipelineJob('Pipeline') {
+  definition {
+    cps {
+      sandbox()
+      script("""
+        node {
+          stage('init') {
+            sh 'hi'
+          } 
+          stage('build') {
+            sh 'how are you'
+          }
+        }
+      """.stripIndent())      
+    }
+  }
+}
