1b4e1bfbf57f4711ef1181b7d85c4ab13167f560
Jayaprakash Reddy <JUSTPERFECT@users.noreply.github.com>
2017-10-27 17:17:19 +0530

    Update iac.groovy

--- iac.groovy
+++ iac.groovy
@@ -13,7 +13,7 @@ pipelineJob('Maven-Test') {
             stage ('Initialize') {
               sh 'mvn package'
               }
-        }
+         }
         """.stripIndent())      
     }
   }
