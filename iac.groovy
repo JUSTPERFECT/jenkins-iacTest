e5ff504c2fec9629d12fb5be0a6905207f6aefae
Jayaprakash Reddy <JUSTPERFECT@users.noreply.github.com>
2017-10-28 23:46:27 +0530

    Update iac.groovy

--- iac.groovy
+++ iac.groovy
@@ -1,4 +1,4 @@
-pipelineJob('Maven-Test') {
+pipelineJob('Restarent-App-build') {
     triggers {
     scm('* * * * *')
   }
@@ -8,13 +8,13 @@ pipelineJob('Maven-Test') {
       script("""
         node('master') {
             stage ('Checkout') {
-               git url: 'https://github.com/JUSTPERFECT/jenkins-maven.git'
+               git url: 'https://github.com/JUSTPERFECT/restarent.git'
                }
             stage ('build') {
               sh '/opt/maven/bin/mvn package'
               }
             stage ('upload to nexus') {
-            nexusArtifactUploader artifacts: [[artifactId: 'newton', classifier: '', file: 'target/newton.war', type: 'war']], credentialsId: 'd4b6cc01-94ed-4024-9dfd-50d99d8fd072', groupId: 'org.restarent.newton', nexusUrl: '54.255.234.64:8081/nexus', nexusVersion: 'nexus2', protocol: 'http', repository: 'releases', version: '${BUILD_NUMBER}'
+            nexusArtifactUploader artifacts: [[artifactId: 'newton', classifier: '', file: 'target/newton.war', type: 'war']], credentialsId: 'd4b6cc01-94ed-4024-9dfd-50d99d8fd072', groupId: 'org.restarent.newton', nexusUrl: '13.229.92.23:8081/nexus', nexusVersion: 'nexus2', protocol: 'http', repository: 'releases', version: '${BUILD_NUMBER}'
               }
          }
         """.stripIndent())      
